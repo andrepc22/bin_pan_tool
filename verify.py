c766ce7647e29d01072e7696dea22ae2d4feb988
Andre Pasache <wandrepc15@gmail.com>
2022-09-18 22:06:54 -0500

    first commit

---
+++ verify.py
@@ -0,0 +1,19 @@
+def validarB02_Pan(cadena):
+
+    thirdOperation = 0
+    if cadena == None:
+        return False
+    longCadena = len(cadena)
+    if not (longCadena==13 or longCadena==16 or longCadena==19):
+        return False
+
+    if longCadena == 16:
+
+    pass
+
+def execution():
+
+    pass
+
+x = input("Pan: ")
+validar_Pan(print(x))
